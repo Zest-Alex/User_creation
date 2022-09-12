42a934c9d56f087b80ad474ad1b9cd18c41a3d87
Zest | Alex <a.tannenberg@outlook.com>
2022-09-12 12:03:20 +0200

    created base for vmcommand

--- create-user.ps1
+++ create-user.ps1
@@ -1,1 +1,14 @@
+Connect-AzAccount
+Set-AzContext "4c28b5e1-69fd-47a6-b80c-f54acda3ec9d"
 
+$rgname = "rgcore"
+$vmname = "srv-verwaltung"
+$spath = ""
+
+Invoke-AzVMRunCommand `
+    -ResourceGroupName $rgname `
+    -VMName $vmname `
+    -CommandId "RunPowerShellScript" `
+    -ScriptPath $spath
+
+#Github path is raw path
